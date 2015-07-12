24a31af7f26d045f9b00a1999c5d844fa71bff4a
Carlos Martín Nieto <cmn@dwim.me>
2015-07-12 19:56:19 +0200

    Revert "Merge branch 'portable-zu'"
    This reverts commit f861abadfe0f1cc4c1b72312e9dd1c7bb39ea42d, reversing
    changes made to a522d8c1e72b3875805750cba9556fd303b9c8b2.

--- src/merge.c
+++ src/merge.c
@@ -72,7 +72,7 @@ int merge_bases_many(git_commit_list **out, git_revwalk **walk_out, git_reposito
 	unsigned int i;
 
 	if (length < 2) {
-		giterr_set(GITERR_INVALID, "At least two commits are required to find an ancestor. Provided 'length' was %" PRIuZ ".", length);
+		giterr_set(GITERR_INVALID, "At least two commits are required to find an ancestor. Provided 'length' was %u.", length);
 		return -1;
 	}
 
@@ -178,7 +178,7 @@ int git_merge_base_octopus(git_oid *out, git_repository *repo, size_t length, co
 	assert(out && repo && input_array);
 
 	if (length < 2) {
-		giterr_set(GITERR_INVALID, "At least two commits are required to find an ancestor. Provided 'length' was %" PRIuZ ".", length);
+		giterr_set(GITERR_INVALID, "At least two commits are required to find an ancestor. Provided 'length' was %u.", length);
 		return -1;
 	}
 
@@ -2408,7 +2408,7 @@ int git_merge__check_result(git_repository *repo, git_index *index_new)
 		goto done;
 
 	if ((conflicts = index_conflicts + wd_conflicts) > 0) {
-		giterr_set(GITERR_MERGE, "%" PRIuZ " uncommitted change%s would be overwritten by merge",
+		giterr_set(GITERR_MERGE, "%d uncommitted change%s would be overwritten by merge",
 			conflicts, (conflicts != 1) ? "s" : "");
 		error = GIT_ECONFLICT;
 	}
