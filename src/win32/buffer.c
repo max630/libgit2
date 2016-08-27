37a56d80802ee662ab3e67e9839fc37bfe72392f
Max Kirillov <max@max630.net>
2016-08-28 01:06:09 +0300

    Revert "Merge pull request #3360 from nodegit/master-duplicate-basename-fix"
    This reverts commit 98f7bd289dfb172473b7d7353d4b0f0b09c67937, reversing
    changes made to cf716beed2f4729cf01a9822d75b4f1ebf7ccbf1.

--- src/win32/buffer.c
+++ src/win32/buffer.c
@@ -6,7 +6,7 @@
  */
 
 #include "common.h"
-#include "w32_buffer.h"
+#include "buffer.h"
 #include "../buffer.h"
 #include "utf-conv.h"
 
@@ -52,3 +52,4 @@ int git_buf_put_w(git_buf *buf, const wchar_t *string_w, size_t len_w)
 	buf->ptr[buf->size] = '\0';
 	return 0;
 }
+
